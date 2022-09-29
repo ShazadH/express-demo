0458889a8484bdf1de3c9c02caf883be342e0924
Shazad Hoang <shazad.hoang@gmail.com>
2022-09-29 16:45:25 +0200

    HTTP PUT and DELETE

--- index.js
+++ index.js
@@ -20,20 +20,14 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("Course not found!!!");
+    if (!course) return res.status(404).send("Course not found!!!");
     res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required(),
-    };
-
-    const result = Joi.validate(req.body, schema);
-    console.log("Result", result);
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -45,8 +39,48 @@ app.post("/api/courses", (req, res) => {
     res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("Course not found!!!");
+
+    //validate course with validateCourse()
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course)
+        return res
+            .status(404)
+            .send("The course with the given ID was not found!!!");
+
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    console.log(index);
+    // console.log(courses.splice(index));
+
+    res.send(course);
+});
+
 // console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`${process.env.USER}: Listening on port ${port}.....`)
 );
+
+function validateCourse(course) {
+    //Validation
+    const schema = {
+        name: Joi.string().min(3).required(),
+    };
+
+    return Joi.validate(course, schema);
+}
